d13bca417d132b2617b3506df0990166d0539f19
noob <mike22070338@gmail.com>
2024-11-02 19:26:23 +0800

    [Fix] More resonable colorschemes.lua config, [ADD] Record.nvim

--- init.lua
+++ init.lua
@@ -959,13 +959,16 @@ require('lazy').setup({
     end,
   },
 
+  { import = 'custom.configs.colorschemes' },
+
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     -- 'olivercederborg/poimandres.nvim',
-    'folke/tokyonight.nvim',
+    -- 'folke/tokyonight.nvim',
+    'neanias/everforest-nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
@@ -973,9 +976,9 @@ require('lazy').setup({
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'nightfox'
       -- vim.cmd.colorscheme 'gruvbox'
-      vim.cmd.colorscheme 'tokyonight'
+      -- vim.cmd.colorscheme 'tokyonight'
 
-      -- vim.cmd.colorscheme 'everforest'
+      vim.cmd.colorscheme 'everforest'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
